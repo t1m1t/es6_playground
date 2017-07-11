4db29fa608c0479b0df6e296e0b2a598871deab1
Tim <timothy.yi7@gmail.com>
2017-07-11 16:04:25 -0700

    super stuff

--- app/index.js
+++ app/index.js
@@ -1,1 +1,45 @@
-console.log('App working: open app/index.js to start exploring ES6');
+
+class Greet {
+    constructor(name){
+        this.name = name;
+    }
+
+    sayHello(){
+        return `Hello ${this.name}`;
+    }
+
+    sayGoodbye(){
+        return `I'll never let go ${this.name}`;
+    }
+}
+
+class BetterGreeting extends Greet {
+    constructor(name, lname) {
+        super(name);
+        this.lastName = lname
+    }
+
+    alertHello(){
+        alert(`What Up, ${this.name} ${this.lastName}`);
+        console.log("alert done");
+    }
+
+    sayHello(){
+        return super.sayHello() + ` ${this.lastName}`;
+    }
+}
+
+const heather = new Greet('Bill');
+const jack = new Greet('Jack');
+
+const bob = new BetterGreeting('Bob', 'Bobby');
+
+console.log(heather.sayHello());
+console.log(heather.sayGoodbye());
+
+console.log(jack.sayHello());
+console.log(jack.sayGoodbye());
+
+console.log(bob.sayHello());
+
+bob.alertHello();
